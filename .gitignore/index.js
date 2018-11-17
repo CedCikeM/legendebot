03936a18b948312790902a46963b20d14b7f6a39
CedCikeM <44348364+CedCikeM@users.noreply.github.com>
2018-11-18 00:51:50 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -12,12 +12,18 @@ bot.login("NDUyODY3MzEwMjQ4Nzg3OTcw.DtIzmw.pMnUAf1cc6nW73IKoMinp7drMTo");
 
 bot.on('message', function (message) {
     if (message.content === '/info') {
-            message.channel.sendMessage("[**information**] \n \n - ``/partenaire`` **Permé de voir les partenaires du serveur**");
+            message.channel.sendMessage("[**information**] \n \n - ``/partenaire`` **Permé de voir les partenaires du serveur** \n\n ``/createur`` **Savoir qui à créer le bot mes on le sais touses");
     }
 } )
 
 bot.on('message', function (message) {
     if (message.content === '/partenaire') {
             message.channel.sendMessage("[**Partenaire**] \n \n Recherche de Partenaire");
+    }
+} )
+
+bot.on('message', function (message) {
+    if (message.content === '/createur') {
+            message.channel.sendMessage("[**__Createur__**] \n \n Mon créateur est belle et bien CedCikeM™#1869 il ma créer pour vous aimers");
     }
 } )
