ea272fa50f93bf82f6186e83fccae05464c9f13d
CedCikeM <44348364+CedCikeM@users.noreply.github.com>
2018-11-18 00:22:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,15 +4,14 @@ const bot = new Discord.Client();
 var prefix = ("/")
 
 bot.on('ready', function() {
-    bot.user.setGame("Légende | /info");
+    bot.user.setGame("/info en DEV")
     console.log("Connectedç");
 });
 
 bot.login("NDUyODY3MzEwMjQ4Nzg3OTcw.DtIzmw.pMnUAf1cc6nW73IKoMinp7drMTo");
 
-
 bot.on('message', function (message) {
-    if (message.content === /info') {
-            message.channel.sendMessage("__Information__ \n \n BOT EN DEVELOPPEMENT");
+    if (message.content === '/info') {
+            message.channel.sendMessage("En Développement \n ========================= ");
     }
 } )
