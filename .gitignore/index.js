7041e0e5b64c0fcaf9418c2851b69844d7c434dc
CedCikeM <44348364+CedCikeM@users.noreply.github.com>
2018-11-18 23:04:02 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,27 +4,14 @@ const bot = new Discord.Client();
 var prefix = ("/")
 
 bot.on('ready', function() {
-    bot.user.setGame("PaguinLand | >help")
+    bot.user.setGame("PaguinLand [1Serveur]")
     console.log("Connectedç");
 });
 
 bot.login("NDUyODY3MzEwMjQ4Nzg3OTcw.DtNyDg.9MI2BRb8alBsBJgfDCZ03FLEVB0");
 
-
-let penguihelp - new discord.RichEmbed()
-
-.setTitle("Penguin Bot Commands")
-.setDescription("All commands of the penguin bot!")
-.addField("Command #1, "Command Description")
-.addField("Command #1, "Command Description")
-.addField("Command #1, "Command Description")
-.addField("Command #1, "Command Description")
-.addField("Command #1, "Command Description");
-
-message.channel.send(penguinhelp);
-
-}
-
-exports.help - {
-name: "help"
-}
+bot.on('message', function (message) {
+    if (message.content === '/help') {
+        message.channel.sendMessage("**__BOT EN DEVELOPPEMENT__**");
+    }
+} )
