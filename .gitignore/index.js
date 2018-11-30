73cb1e11b4636a5d0ea43eacb5537d3337ecac4f
Drakous_Vayne <44348364+CedCikeM@users.noreply.github.com>
2018-11-30 22:12:08 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,7 +10,7 @@ client.login("NDUyODU0Mzg5MzUxMjUxOTg4.DtdcRg.E7UU4v_IYDIQUnGNHmSbl0MWvxE");
 client.on("ready", () => {
 
     console.log("Je suis prêt !");
-    client.user.setGame("");
+    client.user.setGame("1 serveurs list");
 
 });
 
