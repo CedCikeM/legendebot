74204bb01193f6ec354d236094725af91c178a59
Drakous_Vayne <44348364+CedCikeM@users.noreply.github.com>
2019-02-18 17:40:39 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@
  */
 
 // Import the discord.js module
-const Discord = require('discord.');
+const Discord = require('discord.js');
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -17,6 +17,24 @@ client.on("ready", () => {
     console.log("Je suis prêt !");
     client.user.setGame("[/help] Rhilia");
 
+});
+client.on('message', message => {
+  // Voice only works in guilds, if the message does not come from a guild,
+  // we ignore it
+  if (!message.guild) return;
+
+  if (message.content === '/join') {
+    // Only try to join the sender's voice channel if they are in one themselves
+    if (message.member.voiceChannel) {
+      message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+          message.reply('I have successfully connected to the channel!');
+        })
+        .catch(console.log);
+    } else {
+      message.reply('You need to join a voice channel first!');
+    }
+  }
 });
 client.on('guildMemberAdd', member => {
   // Send the message to a designated channel on a server:
