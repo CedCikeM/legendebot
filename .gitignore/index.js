8af51ee9a6751f55f4646e510719f35c719461e5
Drakous_Vayne <44348364+CedCikeM@users.noreply.github.com>
2019-02-18 14:45:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -34,6 +34,13 @@ client.on('message', message => {
   }
 });
 client.on('message', message => {
+  // If the message is "ping"
+  if (message.content === '/update0.1') {
+    // Send "pong" to the same channel
+    message.channel.send('```\nUpdate 0.1 : \n\n -Ajout de la commande /kick : permes de kick un membre du discord \n```\n\n @everyone');
+  }
+});
+client.on('message', message => {
   // Ignore messages that aren't from a guild
   if (!message.guild) return;
 
